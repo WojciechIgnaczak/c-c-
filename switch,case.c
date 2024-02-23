6c885c55850fa4fd6f7008ca389999963cf3f03e
WojciechIgnaczak <wojciech.ignaczak@onet.pl>
2024-02-23 09:22:11 +0100

    switch

---
+++ switch,case.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int x=3;
+    switch(x)
+    {
+        case 1:
+        printf("1");
+        break;
+        case 2:
+        printf("2");
+        break;
+        case 3:
+        printf("3");
+        break;
+    }
+
+    return 0;
+}
