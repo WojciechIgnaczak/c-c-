ed32215c5f42b617870b3ea929c4b6cb50b7faef
WojciechIgnaczak <wojciech.ignaczak@onet.pl>
2024-04-18 10:52:25 +0200

    s

--- statki.c
+++ statki.c
@@ -1,3 +1,11 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
@@ -22,8 +30,15 @@ int main()
             }
         }
     }
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 4; j++) {
+            printf("%d ", t[i][j]);
+        }
+        printf("\n");
+    }
+    printf("\n");
     //czlowiek
-    int c[3][4],m,l;
+    int c[3][4],m=0,k=0,l;
     while (k!=5)
     {
     k=0;
@@ -31,15 +46,22 @@ int main()
     {
         for(int i=0;i<4;i++)
         {
-            l=scanf("%d",c[j][i]);
+            printf("Wprowadź wartość dla c[%d][%d]: ", j, i);
+            l=scanf("%d",&c[j][i]);
             if(l%2==1)
             {
-                m++;
+                k++;
             }
             }
         }
     }
-    
+    k++;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 4; j++) {
+            printf("%d ", c[i][j]);
+        }
+        printf("\n");
+    }
     
     return 0;
 }
